d687fca75e409e79eb58d32c0d328bee33f107d0
Agustin <deadmanwonderland171@gmail.com>
2025-05-31 19:42:32 +0000

    feat: fixed style in favorites

--- public/scripts/renderFavorites.js
+++ public/scripts/renderFavorites.js
@@ -22,8 +22,16 @@ function createMovieCard({ poster_path, title, vote_average, id }) {
               <div class="label">
                 <input type="checkbox" id="checkbox-${id}" data-id="${id}" checked />
                 <div class="heart">
-                  <svg viewBox="0 0 544.582 544.582"><path d="..." /></svg>
-                  <svg viewBox="0 0 544.582 544.582"><path d="..." /></svg>
+                  <svg viewBox="0 0 544.582 544.582">
+                    <path d="M448.069,57.839c-72.675-23.562-150.781,15.759-175.721,87.898C247.41,73.522,169.303,34.277,96.628,57.839
+                    C23.111,81.784-16.975,160.885,6.894,234.708c22.95,70.38,235.773,258.876,263.006,258.876
+                    c27.234,0,244.801-188.267,267.751-258.876C561.595,160.732,521.509,81.631,448.069,57.839z">
+                </svg>
+                <svg viewBox="0 0 544.582 544.582">
+                    <path d="M448.069,57.839c-72.675-23.562-150.781,15.759-175.721,87.898C247.41,73.522,169.303,34.277,96.628,57.839
+                    C23.111,81.784-16.975,160.885,6.894,234.708c22.95,70.38,235.773,258.876,263.006,258.876
+                    c27.234,0,244.801-188.267,267.751-258.876C561.595,160.732,521.509,81.631,448.069,57.839z">
+                </svg>
                 </div>
               </div>
             </label>
@@ -36,6 +44,8 @@ function createMovieCard({ poster_path, title, vote_average, id }) {
       </div>
       <h3 class="movie-card__title"><a href="/movies/${slug}">${title}</a></h3>
     </article>
+
+    
   `;
   return card.firstElementChild;
 }
