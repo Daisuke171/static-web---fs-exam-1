0861dc6bf69137fa0c406657ceebf7bcccead3c8
Agustin <deadmanwonderland171@gmail.com>
2025-06-02 23:07:23 +0000

    feat: used the [page] and [slug] format of Movies and made an entire section only for Actors, also created a ActorsCards with a modification and the slugify script was refactored so the asian characters dont be modificated

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,8 +1,7 @@
 export function slugify(title: string) {
   return title
     .toLowerCase()
-    .replace(/[^\w\s-]/g, '')     // Elimina caracteres especiales
-    .replace(/\s+/g, '-')         // Sustituye espacios por guiones
-    .replace(/--+/g, '-')         // Elimina guiones duplicados
+    .replace(/ +/g, '_')
+    .replace(/_+/g, '-')
     .trim();                      // Quita espacios al principio/final
 }
