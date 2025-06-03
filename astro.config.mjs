060d82f0ab5a7641e3cc3e27723a4372fa3f1712
Agustin <deadmanwonderland171@gmail.com>
2025-06-03 23:09:37 +0000

    mejor vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: { enabled: false},
-    site: 'https://Daisuke171.github.io',
-    base: "/"
+  devToolbar: { enabled: false},
+  adapter: vercel(),
 });
