10c1ab9d7109c9a723990fba0c6106bbb2a73de6
Agustin <deadmanwonderland171@gmail.com>
2025-06-03 21:47:02 +0000

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     devToolbar: { enabled: false},
-    site: 'https://Daisuke171.github.io',
-    base: 'Static-Web---FS-EXAM-1',
+    site: 'https://Daisuke171.github.io'
 });
